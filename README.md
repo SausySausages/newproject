845b98dae670f05ab2b103a870ac27035cdb87d4
SausySausages <143677595+SausySausages@users.noreply.github.com>
2025-12-03 13:38:03 +0000

    datatypeds

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # newproject
+
+java-lessons
---
+++ datatypes.java
@@ -0,0 +1,15 @@
+//java is strongly typed
+
+class DataTypeExamples {
+    int myInt = 42;
+    double myDouble = 182.75;
+    char myChar = 'Q';
+    boolean myBool = false;
+}
+
+public class DataTypes {
+    public static void main(String[] args) {
+        DataTypeExamples dt = new DataTypeExamples();
+        System.out.println("myInt: " + dt.myInt);
+    }
+}
